ed01cd705af34a9921c99723cde0d9d74190ba6c
Abhi <64596494+abhiprojectz@users.noreply.github.com>
2021-03-23 20:00:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 async function main() {
-    await app.listen(3000);
-    console.log('Server on port', 3000);
+    await app.listen(process.env.PORT || 5000);
+    console.log('Server on port',process.env.PORT );
 }
 
 main();
